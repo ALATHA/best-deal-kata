607b8819455786a5ef9a597b6bcac1fa1db18fc9
ALATHA <Nomthandazomde@gmail.com>
2016-03-08 15:52:19 +0200

    test is passing

--- avos.js
+++ avos.js
@@ -2,10 +2,9 @@ function findAvos(number){
 	var avoString ="1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
 	var avocados=avoString.split(",");
 
-	 var avoList =[];
-	 for(var i=0;i<avoString.length;i++){
-		var avo =avoString[i];
-
+// var avo =[];
+for(var i=0;i<avoString.length;i++){
+	var avo =avoString[i];
 	};
 	return avocados;
 }
--- avos_test.js
+++ avos_test.js
@@ -1,5 +1,5 @@
 test("findAvos", function(assert){
 var avoString =findAvos ("1 for R3, 2 for R7, 3 for R10, 5 for R14.50");
-assert.equal(avoString,[]);
+assert.equal(avoString.findAvos);
 });
 
